3b86deddff795438abce4ff48692c7a808666609
Sadanand-developer <sadanand.kumar@mobisec.in>
2024-10-21 11:52:54 +0530

    Update server.js
    change code to deploy

--- server.js
+++ server.js
@@ -1,3 +1,47 @@
+// // const express = require('express');
+// // const WebSocket = require('ws');
+// // const http = require('http');
+
+// // const app = express();
+// // const server = http.createServer(app);
+// // const wss = new WebSocket.Server({ server });
+
+// // const clients = new Map(); // Store connected clients
+
+// // app.use(express.static('public')); // Serve the HTML file
+
+// // wss.on('connection', (ws) => {
+// //     console.log('Client connected');
+
+// //     // Generate a unique ID for each client
+// //     const clientId = Math.random().toString(36).substr(2, 9);
+// //     clients.set(clientId, ws);
+
+// //     // Send the client their unique ID
+// //     ws.send(JSON.stringify({ id: clientId }));
+
+// //     ws.on('message', (message) => {
+// //         const data = JSON.parse(message);
+// //         // Broadcast message to the target client
+// //         if (data.target) {
+// //             const targetClient = clients.get(data.target);
+// //             if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+// //                 targetClient.send(JSON.stringify({ ...data, from: clientId }));
+// //             }
+// //         }
+// //     });
+
+// //     ws.on('close', () => {
+// //         console.log('Client disconnected');
+// //         clients.delete(clientId);
+// //     });
+// // });
+
+// // server.listen(8080, () => {
+// //     console.log('Signaling server is running on http://localhost:8080');
+// // });
+
+
 // const express = require('express');
 // const WebSocket = require('ws');
 // const http = require('http');
@@ -42,45 +86,55 @@
 // });
 
 
-const express = require('express');
+
+
+
 const WebSocket = require('ws');
-const http = require('http');
 
-const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+let clients = new Map(); // Store connected clients
 
-const clients = new Map(); // Store connected clients
+module.exports = (req, res) => {
+    if (req.method === 'GET' && req.url === '/ws') {
+        const wss = new WebSocket.Server({ noServer: true });
 
-app.use(express.static('public')); // Serve the HTML file
+        // Handle WebSocket connections
+        wss.on('connection', (ws) => {
+            console.log('Client connected');
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
+            // Generate a unique ID for each client
+            const clientId = Math.random().toString(36).substr(2, 9);
+            clients.set(clientId, ws);
 
-    // Generate a unique ID for each client
-    const clientId = Math.random().toString(36).substr(2, 9);
-    clients.set(clientId, ws);
+            // Send the client their unique ID
+            ws.send(JSON.stringify({ id: clientId }));
 
-    // Send the client their unique ID
-    ws.send(JSON.stringify({ id: clientId }));
+            ws.on('message', (message) => {
+                const data = JSON.parse(message);
+                // Broadcast message to the target client
+                if (data.target) {
+                    const targetClient = clients.get(data.target);
+                    if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+                        targetClient.send(JSON.stringify({ ...data, from: clientId }));
+                    }
+                }
+            });
 
-    ws.on('message', (message) => {
-        const data = JSON.parse(message);
-        // Broadcast message to the target client
-        if (data.target) {
-            const targetClient = clients.get(data.target);
-            if (targetClient && targetClient.readyState === WebSocket.OPEN) {
-                targetClient.send(JSON.stringify({ ...data, from: clientId }));
-            }
-        }
-    });
+            ws.on('close', () => {
+                console.log('Client disconnected');
+                clients.delete(clientId);
+            });
+        });
 
-    ws.on('close', () => {
-        console.log('Client disconnected');
-        clients.delete(clientId);
-    });
-});
+        req.socket.server.on('upgrade', (request, socket, head) => {
+            wss.handleUpgrade(request, socket, head, (ws) => {
+                wss.emit('connection', ws, request);
+            });
+        });
 
-server.listen(8080, () => {
-    console.log('Signaling server is running on http://localhost:8080');
-});
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('WebSocket server is running');
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+};
